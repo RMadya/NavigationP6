f7ba425e3e8dcd5202f6602df084b7709641f0bb
RMadya <kereyoben75@gmail.com>
2025-11-06 16:06:53 +0700

    menampilkan kolom input teks dengan label "Alamat" dan lebar tetap 250.dp. Namun, karena singleLine adalah true, kolom ini hanya cocok untuk alamat yang sangat singkat.

--- app/src/main/java/com/example/navigationp6/Formulir.kt
+++ app/src/main/java/com/example/navigationp6/Formulir.kt
@@ -74,7 +74,13 @@ fun FormIsian(
                 thickness = 1.dp,
                 color = Color.Red
             )
-
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                label = {Text(text = "Alamat")},
+                onValueChange = {},
             )
         }
     }
